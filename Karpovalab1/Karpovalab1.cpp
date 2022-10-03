dbe40495926e42a5d228aa2caba97f6dc07a4793
DashaKarpova <112941801+DashaKarpova@users.noreply.github.com>
2022-10-03 23:54:30 +0300

    option check, edit cin

--- Karpovalab1/Karpovalab1.cpp
+++ Karpovalab1/Karpovalab1.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//создание структур (труба и компрессорная станция)
 struct Pipe
 {
 	float lenght = 0, diameter = 0;
@@ -17,77 +18,85 @@ struct ComSt
 	float effeciency = 0; 
 };
 
-void error(int i) { 
-	while ((!i)|| (i<=0)) {
-		cout << "Error!!! Input numeric value" << endl;
+//ошибка при вводе значений
+float error(float i) { 
+	while ((((cin>>i).fail())||(cin.peek() != '\n'))||(i<=0)) {
+		cout << "Error!!! Input numeric value>0" << endl;
 		cin.clear();
 		cin.ignore(INT_MAX, '\n');
-		cin >> i;
+		
 	}
+	return i;
 }
 
-
-string status(int x) {
-	if (x == 2)
-		return ("Pipe works");
-	else if (x == 1)
-		return ("Pipe is repairing");
-	else
-		return ("Unknown");
-}
-
-int status_cin(int x){
-	while ((x > 2) || (!x) || (x <= 0)) {
-		cout << "Error!!! Input numeric value" << endl;
+//проверка 
+int check_option(int x) {
+	while (((cin >> x).fail()) || (cin.peek() != '\n')) {
+		cout << "Input correct number(0-7)" << endl;
 		cin.clear();
 		cin.ignore(INT_MAX, '\n');
-		cin >> x;
+	}
+	return x;
+}
+//проверка статуса
+void status(bool status) {
+	if (status == true)
+		cout<<"\nPipe works";
+	else 
+		cout<<"\nPipe is repairing";
+}
+
+bool status_cin(bool checkstatus) {
+	while (((cin >> checkstatus).fail()) || (cin.peek() != '\n'))
+	{
+		cout << "Error!\nInput one of the predefined states:\n 0.In repair\n 1.In work\n ";
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+	}
+	return checkstatus;
+}
+
+//Проверка цехов
+int integervalue(int x) {
+	while ((((cin >> x).fail()) || (cin.peek() != '\n')) ||(x / trunc(x) != 1)||(x<=0)) {
+		cout << "Error!!! Input integer value" << endl;
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
 	}
 	return x;
 }
 
 int workshop_cin(int x, int y) {
-	while ((x > y) || (!x) || (x <= 0)) {
+	while ((((cin>>x).fail())||(cin.peek()!='\n')) || (x > y) || (x <= 0)) {
 		cout << "Error!!! Input numeric value" << endl;
 		cin.clear();
 		cin.ignore(INT_MAX, '\n');
-		cin >> x;
 	}
 	return x;
 }
 
-double integervalue(double x) {
-	while (x / trunc(x) != 1) {
-		cout << "Error!!! Input integer value" << endl;
-		cin.clear();
-		cin.ignore(INT_MAX, '\n');
-		cin >> x;
-	}
-	return x;
-}
+
 
 void Addpipe(Pipe& p)
 {
 	cout << "\nInput lenght";
-	cin >> p.lenght;
-	error(int(p.lenght));
+	p.lenght=error(p.lenght);
 	cout << "\nInput diameter";
-	cin >> p.diameter;
-	error(int(p.diameter));
+	p.diameter = error(p.diameter);
 	cout << "\nChoose status of pipe(1-if repairing, 2 if works)";
-	cin >> p.status;
 	p.status = status_cin(p.status);
-	cout << status(p.status) << endl;
+	//status(p.status);
+	 status(p.status);
 }
 
 void AddCS(ComSt& cs) {
 	cout << "\nInput name";
-	cin >> cs.name;
+	cin.clear();
+	cin.ignore(INT_MAX, '\n');
+	getline(cin, cs.name);
 	cout << "\nNumber_of_workshop";
-	cin >> cs.workshop;
 	cs.workshop = integervalue(cs.workshop);
 	cout << "\nNumber_of_Working_workshop";
-	cin >> cs.working_ws;
 	cs.working_ws = integervalue(cs.working_ws);
 	cs.working_ws = workshop_cin(cs.working_ws, cs.workshop);
 	cs.effeciency = float(cs.working_ws) / float(cs.workshop) * 100;
@@ -96,7 +105,7 @@ void AddCS(ComSt& cs) {
 
 void Viewall(Pipe& p, ComSt& cs) {
 	cout << "\nPipe info:\nLenght: " << p.lenght << "\nDiameter:" << p.diameter
-		<< "\nStatus:" << status(p.status) << endl;
+		<< "\nStatus:"; status(p.status);
 	cout << "\nComressor Station info:\nName: " << cs.name << "\nNumber of workshops: " << cs.workshop
 		<< "\nNumber of working workshops: " << cs.working_ws << "\nEfficiency: " << cs.effeciency << "%" << endl;
 }
@@ -108,7 +117,7 @@ void Editpipe(Pipe& p) {
 		cout << "\nInput new status of pipe (1-repairing, 2-working)" << endl;
 		cin >> p.status;
 		p.status = status_cin(p.status);
-		cout << status(p.status) << endl;
+		status(p.status);
 	}
 }
 
@@ -143,20 +152,24 @@ void loadfromfile(Pipe& p, ComSt& cs) {
 	ifstream file2;
 	string line;
 	file2.open("Data.txt");
-	getline(file2, line);
-	p.lenght = stoi(line);
-	getline(file2, line);
-	p.diameter = stoi(line);
-	getline(file2, line);
-	p.status = stoi(line);
-	getline(file2, line);
-	cs.name = line;
-	getline(file2, line);
-	cs.workshop = stoi(line);
-	getline(file2, line);
-	cs.working_ws = stoi(line);
-	getline(file2, line);
-	cs.effeciency = stod(line);
+	if (file2.is_open()) {
+		getline(file2, line);
+		p.lenght = stoi(line);
+		getline(file2, line);
+		p.diameter = stoi(line);
+		getline(file2, line);
+		p.status = stoi(line);
+		getline(file2, line);
+		cs.name = line;
+		getline(file2, line);
+		cs.workshop = stoi(line);
+		getline(file2, line);
+		cs.working_ws = stoi(line);
+		getline(file2, line);
+		cs.effeciency = stod(line);
+	}
+	else
+		cout << "file doesn't exist";
 }
 int main() {
 	int option = -1;
@@ -165,7 +178,7 @@ int main() {
 
 	while (option) {
 		cout << "\nChoose Option:\n 1.Add pipe 2. Add compressor station 3.View all objects 4. Edit pipe 5. Edit Comressor Station 6. Save 7. Load 0. Exit\n";
-		cin >> option;
+		option = check_option(option);
 		switch (option) {
 		case 1: {
 			Addpipe(p);
